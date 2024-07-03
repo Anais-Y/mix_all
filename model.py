bad3f259277bebbc295eacdf4eea193a3d93f05b
Anaiis <18241082@buaa.edu.cn>
2024-07-03 23:45:00 +0800

    0703

--- model.py
+++ model.py
@@ -14,25 +14,28 @@ class GAT(nn.Module):
         self.num_heads = num_heads
         self.conv1 = GATConv(num_node_features, hidden_dim, heads=num_heads, dropout=dropout_disac)
         self.conv2 = GATConv(hidden_dim * num_heads, 24, heads=1, concat=False, dropout=dropout_disac)
-        self.conv3 = GATConv(num_node_features, num_classes, heads=num_heads, concat=False, dropout=dropout_disac)
+        self.conv3 = GATConv(num_node_features, 24, heads=num_heads, concat=False, dropout=dropout_disac)
         self.bn1 = BatchNorm1d(num_node_features)
         self.bn2 = BatchNorm1d(hidden_dim * num_heads)
         self.conv_normal = Conv1d(in_channels=24, out_channels=8, kernel_size=62)
+        nn.init.kaiming_uniform_(self.conv3.lin.weight)
+        nn.init.xavier_uniform_(self.conv3.att_src)
+        nn.init.xavier_uniform_(self.conv3.att_dst)
         # self.fc = nn.Linear(out_features * num_features, out_features)
 
     def forward(self, band_data):
         x, edge_index, batch = band_data.x, band_data.edge_index, band_data.batch
         batch_size = batch.max().item() + 1
         # 第一层GAT卷积
-        x = F.dropout(x, p=0.6)
+        # x = F.dropout(x, p=0.6)
         x = self.bn1(x)
         # print('before gconv1:', x.shape)
-        x = self.conv1(x, edge_index)
-        x = self.conv2(x, edge_index)
+        x = self.conv3(x, edge_index)
+        # x = self.conv2(x, edge_index)
         x = F.elu(x)
         # print('after gconv1:', x.shape)  # (2976, hidden_dim)
         
-        x = x.view(batch_size, 62*3, -1)
+        x = x.reshape(batch_size, 62*3, -1)
         
         x = x.permute(0, 2, 1)  # (batch_size, num_features, num_channels)
         # print(x.shape)
@@ -72,6 +75,21 @@ class SelfAttention(nn.Module):
         return torch.matmul(attention_scores, V)
     
 
+class MultiH_Attention(nn.Module):
+    def __init__(self, embed_size, heads):
+        super(MultiH_Attention, self).__init__()
+        self.multihead_attn = nn.MultiheadAttention(embed_dim=embed_size, num_heads=heads)
+        self.norm = nn.LayerNorm(embed_size)  # 添加 Layer Normalization 层
+
+    def forward(self, x):
+        # 注意力机制要求输入形式为 (seq_len, bs, embed_size)
+        x = self.norm(x)
+        x = x.permute(1, 0, 2)  # 转换输入为适合 PyTorch 注意力模块的维度
+        attn_output, _ = self.multihead_attn(x, x, x)
+        attn_output = attn_output.permute(1, 0, 2)  # 将输出转换回原始的维度
+        return attn_output
+   
+
 class FusionModel(nn.Module):
     def __init__(self, num_node_features, hidden_dim, num_heads, dropout_disac, num_classes, dataset):
         super(FusionModel, self).__init__()
@@ -89,12 +107,12 @@ class FusionModel(nn.Module):
         if self.dataset == "DEAP":
             self.fusion_gat = nn.Linear(4 * num_classes, num_classes, bias=True)
         elif self.dataset == "SEED":
-            self.fusion_gat = nn.Linear(40*125, 512, bias=True)
+            self.fusion_gat = MultiH_Attention(embed_size=125, heads=5)
         else:
             raise ValueError("Please give a dataset")
-        nn.init.kaiming_uniform_(self.fusion_gat.weight, nonlinearity='relu')
+        # nn.init.kaiming_uniform_(self.fusion_gat.weight, nonlinearity='relu')
         # self.de_encoder = SelfAttention(310*3, 64)  # 62*5*3
-        self.fusion_all = SelfAttention(512, num_classes)
+        self.fusion_all = nn.Linear(40*125, num_classes)
         self.ln = nn.LayerNorm(310*3)
 
     def forward(self, data):
@@ -108,7 +126,8 @@ class FusionModel(nn.Module):
             x_delta = self.GAT_delta(data['delta'])
             x_concat = torch.cat((x_delta, x_alpha, x_beta, x_gamma, x_theta), dim=1)
             # print(data['alpha'], x_delta.shape, x_concat.shape)
-            x_concat = x_concat.view(x_concat.size(0), -1)
+            x_concat = self.fusion_gat(x_concat)
+            x_concat = x_concat.reshape(x_concat.size(0), -1)
         else:
             print('[Attention]!!!')
             x_concat = torch.cat((x_alpha, x_beta, x_gamma, x_theta), dim=1)
@@ -123,8 +142,7 @@ class FusionModel(nn.Module):
         # x_de = F.leaky_relu(x_de, negative_slope=1)
         # print(x_de.shape)
         # x_out = self.fusion_all(torch.cat((x_out, x_de), dim=1))
-        x_out = self.fusion_gat(x_concat)
-        x_out = self.fusion_all(x_out)
+        x_out = self.fusion_all(x_concat)
         return F.leaky_relu(x_out)
 
 
